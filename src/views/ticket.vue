1223e681ab7ddbb30c9f883d38b3ff204bb81c19
zxc <1204607112@qq.com>
2019-05-09 10:16:07 +0800

    header做完

--- src/views/ticket.vue
+++ src/views/ticket.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <!-- ticket
+  <!-- ticket
     <ul>
       <router-link to='/detail/id'
                    tag='li'
@@ -11,17 +10,16 @@
                    tag='li'
                    activeClass="active">跳转具体影城页</router-link>
     </ul> -->
-    <nav class="nav">
-      <div>
-        <a href=""><i></i></a>
-      </div>
-      <h1 class="Tnav-header">{{this.datalist.nm}}</h1>
-    </nav>
-
-  </div>
+  <header class="Tnav">
+    <div class="Tnav-left">
+      <a href="#"><i class="iconfont icon-zuojiantou"></i></a>
+    </div>
+    <h1 class="Tnav-header">{{this.datalist.nm}}</h1>
+  </header>
 
 </template>
 <script>
+import icon from '../../public/icon/iconfont.css'
 import axios from 'axios'
 export default {
   data () {
@@ -40,17 +38,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.nav {
+* {
+  margin: 0;
+  padding: 0;
+}
+img {
+  display: block;
+}
+ul,
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  cursor: pointer;
+}
+.Tnav {
   height: 50.5px;
   color: #fff;
   background: #e54847;
   border-bottom: 1px solid #e54847;
   position: relative;
   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  overflow: hidden;
 }
 .Tnav-header {
-  display: block;
+  display: inline-block;
   font-size: 18px;
+  -webkit-box-flex: 1;
   font-weight: 400;
   text-align: center;
   line-height: 50px;
@@ -58,5 +74,26 @@ export default {
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+  float: right;
+}
+.Tnav-left {
+  height: 50.5px;
+  line-height: 50.5px;
+  width: 50px;
+  float: left;
+}
+.Tnav-left a {
+  height: 50px;
+  width: 22.5px;
+  line-height: 50px;
+  padding: 0 15px;
+  display: inline-block;
+}
+.icon-zuojiantou {
+  display: inline-block;
+  width: 22.5px;
+  height: 22.5px;
+  vertical-align: middle;
+  position: relative;
 }
 </style>
