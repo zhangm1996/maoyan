6ba6c9d0a8cf407e455f7285cdbd1add48e2ea13
zxc <1204607112@qq.com>
2019-05-09 10:03:05 +0800

    数据请求完成

--- src/views/ticket.vue
+++ src/views/ticket.vue
@@ -1,11 +1,62 @@
 <template>
-    <div>
-        ticket
-        <ul>
-            <router-link to='/detail/id' tag='li' activeClass="active">跳转详情页</router-link>
-        </ul>
-        <ul>
-            <router-link to='/studios/id' tag='li' activeClass="active">跳转具体影城页</router-link>
-        </ul>
-    </div>
+  <div>
+    <!-- ticket
+    <ul>
+      <router-link to='/detail/id'
+                   tag='li'
+                   activeClass="active">跳转详情页</router-link>
+    </ul>
+    <ul>
+      <router-link to='/studios/id'
+                   tag='li'
+                   activeClass="active">跳转具体影城页</router-link>
+    </ul> -->
+    <nav class="nav">
+      <div>
+        <a href=""><i></i></a>
+      </div>
+      <h1 class="Tnav-header">{{this.datalist.nm}}</h1>
+    </nav>
+
+  </div>
+
 </template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      datalist: {}
+    }
+  },
+  created () {
+    axios({
+      url: '/ajax/detailmovie?movieId=248172',
+    }).then(res => {
+      this.datalist = res.data.detailMovie
+      console.log(res.data.detailMovie)
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
+.nav {
+  height: 50.5px;
+  color: #fff;
+  background: #e54847;
+  border-bottom: 1px solid #e54847;
+  position: relative;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+}
+.Tnav-header {
+  display: block;
+  font-size: 18px;
+  font-weight: 400;
+  text-align: center;
+  line-height: 50px;
+  margin: 0 52.5px 0 0;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+</style>
